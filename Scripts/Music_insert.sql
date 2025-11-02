a264e69f600d690ee43bddeedd8ff0709b733cf7
Roman E <rk2555@yandex.ru>
2025-11-02 15:26:32 +0300

    Format music_insert

--- Scripts/Music_insert.sql
+++ Scripts/Music_insert.sql
@@ -45,27 +45,83 @@ VALUES ('Лучшая классика русского рока', 2018),
 
 -- связи исполнители и жанры
 INSERT INTO Artist_Genre(artist_id, genre_id)
-VALUES ((SELECT artist_id FROM Artists WHERE name = 'Кино'), (SELECT genre_id FROM Genres WHERE name = 'Русский рок')),
-       ((SELECT artist_id FROM Artists WHERE name = 'ДДТ'), (SELECT genre_id FROM Genres WHERE name = 'Классический рок')),
-       ((SELECT artist_id FROM Artists WHERE name = 'Виктор Цой'), (SELECT genre_id FROM Genres WHERE name = 'Русский рок')),
-       ((SELECT artist_id FROM Artists WHERE name = 'Сергей Лазарев'), (SELECT genre_id FROM Genres WHERE name = 'Поп-музыка')),
-       ((SELECT artist_id FROM Artists WHERE name = 'Михаил Круг'), (SELECT genre_id FROM Genres WHERE name = 'Шансон')),
-       ((SELECT artist_id FROM Artists WHERE name = 'Ночные Снайперы'), (SELECT genre_id FROM Genres WHERE name = 'Русский рок')),
-       ((SELECT artist_id FROM Artists WHERE name = 'Стас Михайлов'), (SELECT genre_id FROM Genres WHERE name = 'Шансон'));
+VALUES
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Кино'),
+	(SELECT genre_id FROM Genres WHERE name = 'Русский рок')
+),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'ДДТ'),
+	(SELECT genre_id FROM Genres WHERE name = 'Классический рок')
+),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Виктор Цой'),
+	(SELECT genre_id FROM Genres WHERE name = 'Русский рок')),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Сергей Лазарев'),
+	(SELECT genre_id FROM Genres WHERE name = 'Поп-музыка')
+),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Михаил Круг'),
+	(SELECT genre_id FROM Genres WHERE name = 'Шансон')
+),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Ночные Снайперы'),
+	(SELECT genre_id FROM Genres WHERE name = 'Русский рок')
+),
+(
+	(SELECT artist_id FROM Artists WHERE name = 'Стас Михайлов'),
+	(SELECT genre_id FROM Genres WHERE name = 'Шансон')
+);
 
 -- связи исполнители и альбомы
 INSERT INTO Album_Artist(album_id, artist_id)
-VALUES ((SELECT album_id FROM Albums WHERE title = 'Группа крови'), (SELECT artist_id FROM Artists WHERE name = 'Кино')),
-       ((SELECT album_id FROM Albums WHERE title = 'Что такое осень?'), (SELECT artist_id FROM Artists WHERE name = 'ДДТ')),
-       ((SELECT album_id FROM Albums WHERE title = 'Нашествие'), (SELECT artist_id FROM Artists WHERE name = 'Виктор Цой')),
-       ((SELECT album_id FROM Albums WHERE title = 'Мир без войны'), (SELECT artist_id FROM Artists WHERE name = 'Сергей Лазарев')),
-       ((SELECT album_id FROM Albums WHERE title = 'Горький мёд'), (SELECT artist_id FROM Artists WHERE name = 'Михаил Круг')),
-       ((SELECT album_id FROM Albums WHERE title = 'Без меня'), (SELECT artist_id FROM Artists WHERE name = 'Ночные Снайперы')),
-       ((SELECT album_id FROM Albums WHERE title = 'Жизнь удалась'), (SELECT artist_id FROM Artists WHERE name = 'Стас Михайлов'));
+VALUES
+(
+	(SELECT album_id FROM Albums WHERE title = 'Группа крови'),
+	(SELECT artist_id FROM Artists WHERE name = 'Кино')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Что такое осень?'),
+	(SELECT artist_id FROM Artists WHERE name = 'ДДТ')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Нашествие'),
+	(SELECT artist_id FROM Artists WHERE name = 'Виктор Цой')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Мир без войны'),
+	(SELECT artist_id FROM Artists WHERE name = 'Сергей Лазарев')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Горький мёд'),
+	(SELECT artist_id FROM Artists WHERE name = 'Михаил Круг')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Без меня'),
+	(SELECT artist_id FROM Artists WHERE name = 'Ночные Снайперы')
+),
+(
+	(SELECT album_id FROM Albums WHERE title = 'Жизнь удалась'),
+	(SELECT artist_id FROM Artists WHERE name = 'Стас Михайлов')
+);
 
 -- связи сборники и треки
 INSERT INTO Playlist_Track(playlist_id, track_id)
-VALUES ((SELECT playlist_id FROM Playlists WHERE title = 'Лучшая классика русского рока'), (SELECT track_id FROM Tracks WHERE title = 'Группа крови')),
-       ((SELECT playlist_id FROM Playlists WHERE title = 'Русская попса'), (SELECT track_id FROM Tracks WHERE title = 'Моя Москва')),
-       ((SELECT playlist_id FROM Playlists WHERE title = 'Шансон хит-парад'), (SELECT track_id FROM Tracks WHERE title = 'Буду пьяным')),
-       ((SELECT playlist_id FROM Playlists WHERE title = 'Лучший шансон 2019'), (SELECT track_id FROM Tracks WHERE title = 'Осень'));
+VALUES
+(
+	(SELECT playlist_id FROM Playlists WHERE title = 'Лучшая классика русского рока'),
+	(SELECT track_id FROM Tracks WHERE title = 'Группа крови')
+),
+(
+	(SELECT playlist_id FROM Playlists WHERE title = 'Русская попса'),
+	(SELECT track_id FROM Tracks WHERE title = 'Моя Москва')
+),
+(
+	(SELECT playlist_id FROM Playlists WHERE title = 'Шансон хит-парад'),
+	(SELECT track_id FROM Tracks WHERE title = 'Буду пьяным')
+),
+(
+	(SELECT playlist_id FROM Playlists WHERE title = 'Лучший шансон 2019'),
+	(SELECT track_id FROM Tracks WHERE title = 'Осень')
+);
